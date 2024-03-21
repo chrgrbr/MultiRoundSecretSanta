39598a8b5c37ea69de2920f001feaa5fcf5041f4
chrgrbr <chrgr@gmx.de>
2024-03-21 21:05:39 +0100

    intial commit

---
+++ draw.py
@@ -0,0 +1,83 @@
+import random
+import yaml
+import json
+from send import send_email, move_email_to_archive
+
+
+MAX_ATTEMPTS = 20
+
+def load_data(filename):
+    with open(filename, 'r') as file:
+        return yaml.safe_load(file)
+    
+def load_mail_contacts(filename): 
+    with open(filename, 'r') as file:
+        return json.load(file)
+    
+
+def draw(names, exclusions=None, previous_pairings=None):
+    attempts =0
+    if exclusions is None:
+        exclusions = {}
+    if previous_pairings is None:
+        previous_pairings = {}
+        
+    receivers = names[:]
+    givers = names[:]
+    random.shuffle(receivers)
+    pairs = {}
+    
+    for giver in givers:
+        receiver = random.choice(receivers)
+        while receiver == giver \
+            or (giver in exclusions and receiver in exclusions[giver]) \
+            or (previous_pairings.get(giver) == receiver) \
+            or (previous_pairings.get(receiver) == giver):
+            receiver = random.choice(receivers)
+            if attempts > MAX_ATTEMPTS: #RECURSIVE DOOM
+                return draw(names, exclusions, previous_pairings)
+            else:
+                attempts += 1 
+        pairs[giver] = receiver
+        receivers.remove(receiver)
+    
+    return pairs
+
+def emails_for_pairings(pairings, stage):
+    emails = []
+    for giver, receiver in pairings.items():
+        emails.append(
+            (giver, receiver, f"Runde {stage}: {giver}, du hast {receiver} gezogen!")
+        )
+    return emails
+
+
+
+def main():
+    data = load_data("secret_santa.yaml")
+    mail_contacts = load_mail_contacts("mail_contacts.json")
+
+    previous_pairings = {}
+    final_pairings = []
+    
+    for i, draw_info in enumerate(data["draws"], start=1):
+        participants = draw_info["participants"]
+        exclusions = draw_info.get("exclusions", {})
+        pairs = draw(participants, exclusions, previous_pairings)
+        print(f"Draw {i}:")
+        for giver, receiver in pairs.items():
+            print(f"{giver} -> {receiver}")
+        previous_pairings.update(pairs)
+        final_pairings.append(pairs)
+
+    emails = []
+    for i, pairings in enumerate(final_pairings, start=1):
+        emails.extend(emails_for_pairings(pairings, i))
+    print("Sending emails...")
+    
+    for giver, receiver, body in emails:
+        send_email(mail_contacts[giver], body, body)
+        move_email_to_archive(mail_contacts[giver])
+
+if __name__ == "__main__":
+    main()
---
+++ mail_utils.py
@@ -0,0 +1,32 @@
+import base64
+from email.message import EmailMessage
+import os
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2.credentials import Credentials
+
+
+creds = Credentials.from_authorized_user_file("token.json")
+service = build("gmail", "v1", credentials=creds)
+
+
+def send_email(recipent, subject, body, from_email="derkahlehorst@gmail.com"):
+    msg = EmailMessage()
+    msg["to"] = recipent
+    msg["from"] = from_email
+    msg["subject"] = subject
+    msg.set_content(body)
+    try:
+        message = service.users().messages().send(userId="me", body={"raw": base64.urlsafe_b64encode(msg.as_bytes()).decode()}).execute()
+        return True, message
+    except HttpError as error:
+        return False, error
+    
+def move_email_to_archive(email_id):
+    try:
+        message = service.users().messages().modify(userId="me", id=email_id, body={"removeLabelIds": ["SENT"], "addLabelIds": ["Label_4322346357244942836"]}).execute()
+        return True, message
+    except HttpError as error:
+        return False, error
+    
